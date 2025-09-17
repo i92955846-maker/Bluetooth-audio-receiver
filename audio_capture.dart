a7bb8474eb96297d9c196a8c236efec19efc4d4a
i92955846-maker <i92955846@gmail.com>
2025-09-18 01:00:04 +0530

    Delete audio_capture.dart

--- audio_capture.dart
+++
@@ -1,4 +0,0 @@
-// Placeholder audio capture helper (use flutter_sound in real project)
-import 'package:flutter/material.dart';
-typedef OnPCM = void Function(List<int> pcm);
-class AudioCapture { static Future<void> startSendingToBluetooth(OnPCM cb) async{} static Future<void> stopSendingToBluetooth() async{} static void dispose(){} }
