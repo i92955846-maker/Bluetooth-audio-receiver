1e770a4151816979e901aff396affa76a13b38af
i92955846-maker <i92955846@gmail.com>
2025-09-18 00:59:34 +0530

    Delete permissions_helper.dart

--- permissions_helper.dart
+++
@@ -1,7 +0,0 @@
-import 'package:permission_handler/permission_handler.dart';
-class PermissionsHelper {
-  static Future<void> requestPermissions() async {
-    final perms = [Permission.bluetooth, Permission.bluetoothScan, Permission.bluetoothConnect, Permission.microphone, Permission.storage, Permission.locationWhenInUse, Permission.phone];
-    for (final p in perms) { try { await p.request(); } catch (_) {} }
-  }
-}
